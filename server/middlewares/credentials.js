220a1079f3d81f0581832e0e267d432c5e31c319
khantmin <kmin01405@gmail.com>
2023-03-09 09:51:16 +0630

    fixed bugs

--- server/middlewares/credentials.js
+++ server/middlewares/credentials.js
@@ -4,6 +4,7 @@ const creditials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin: *");
   }
   next();
 };
