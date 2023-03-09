5dec2aa3779adf2ecff98d7692b69e1dfe9d37d5
khantmin <kmin01405@gmail.com>
2023-03-09 10:54:00 +0630

    fixed bugs

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
   "http://localhost:3500/",
   "http://localhost:5173",
   "https://form-authentication-orpin.vercel.app",
+  "https://form-authentication-orpin.vercel.app/",
 ];
 module.exports = allowedOrigins;
